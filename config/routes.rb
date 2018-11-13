dfe6e48f0f7d5dbeb2d7b3d67de1a0005db122b3
TinQk <potie.quentin@gmail.com>
2018-11-14 00:09:44 +0100

    page12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/page1'
+  get 'home/page2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
